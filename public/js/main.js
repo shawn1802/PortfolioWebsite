b01a2d596e44dfdb71798588b5e2ce7c05de4207
shawn1802 <e0958597@u.nus.edu>
2024-02-26 15:43:20 +0800

    alignment updates

--- public/js/main.js
+++ public/js/main.js
@@ -149,7 +149,7 @@
 // makes skills-tab functional
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname) {
+function opentab(tabname, event) {
   for (tablink of tablinks) {
     tablink.classList.remove("active-link");
   }
